f1cf184fbd6eb4268056528ad4c1380aff821e27
Mike Walker <mikewalker24@gmail.com>
2025-09-21 00:14:02 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,49 @@
 import terser from '@rollup/plugin-terser';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const output = [
+  {
+    file: 'dist/index.js',
+    format: 'es',
+    sourcemap: true,
+  },
+  {
+    file: 'dist/index.mjs',
+    format: 'es',
+    sourcemap: true,
+  },
+  {
+    name: 'animateWhenVisible',
+    file: 'dist/index.umd.js',
+    format: 'umd',
+    sourcemap: true,
+  },
+];
+
+const productionOutput = [
+  {
+    file: 'dist/index.min.js',
+    format: 'es',
+    sourcemap: true,
+    plugins: [terser({ keep_fnames: true })],
+  },
+  {
+    file: 'dist/index.min.mjs',
+    format: 'es',
+    sourcemap: true,
+    plugins: [terser({ keep_fnames: true })],
+  },
+  {
+    name: 'animateWhenVisible',
+    file: 'dist/index.umd.min.js',
+    format: 'umd',
+    sourcemap: true,
+    plugins: [terser({ keep_fnames: true })],
+  },
+];
+
 export default {
   input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/index.js', // ESM build
-      format: 'es',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    terser({
-      keep_fnames: true, // preserves function names for easier debugging
-    }),
-  ],
+  output: isProduction ? [...output, ...productionOutput] : output,
 };
